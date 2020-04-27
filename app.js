cc39003d668ad64c5fc0f76dca0b42675ba423a1
Vinh Le <vinh@vertics.co>
2020-04-27 17:06:33 +0300

    refactor: add IP address of ec2 instance

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello from EC2'))
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () => console.log(`Example app listening on port ${port}`))
